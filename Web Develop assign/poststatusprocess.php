379a52cd71a3488e2b86c1a4ea2a26d67b6a0470
Yu8888 <65652928+Yu8888@users.noreply.github.com>
2021-04-18 16:27:48 +1200

    PHP Web

---
+++ Web Develop assign/poststatusprocess.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=P, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="content">
+    <?php
+    $sql_host = "cmslamp14.aut.ac.nz";
+    $sql_user = "yxn9194";
+    $sql_pass = "";
+    $sql_db = "yxn9194";
+
+    $c = $_POST["statuscode"];
+    $pattern1 = "/^S{1}[0-9]{4}$/";
+    $d = $_POST["status"];
+    $pattern = "/^[a-zA-z0-9., ?!]+$/";
+    if (preg_match($pattern1, $c) == 0 && preg_match($pattern, $d) == 1) {
+        echo "Status code:Please input 'S'and followed with four numbers!";
+    } else if (preg_match($pattern1, $c) == 1 && preg_match($pattern, $d) == 0) {
+        echo "Status:Please input alphanumeric characters including spaces, comma, period (full stop), 
+        exclamation point and question mark only!";
+    } else if (preg_match($pattern1, $c) == 0 && preg_match($pattern, $d) == 0) {
+        echo "Status code:Please input 'S'and followed with four numbers.<br>
+              Status:Please input alphanumeric characters including spaces, comma, period (full stop), 
+              exclamation point and question mark only!";
+    } 
+
+    else {
+        $hobby_arr = array();
+        $hobby_arr = $_POST['hobby'];
+                $hobby = implode('、', $hobby_arr);
+                $m = $_POST["statuscode"];
+                $n = $_POST["status"];
+                $p = $_POST["sex"];
+                $o = $_POST["Date"];
+                echo "Post successfully!<br>
+                      Your Status Code: $m
+                      Your Status: $n
+                      Share: $p
+                      Date: $o
+                      Permission type: $hobby";
+        ?>
+        </div>
+        <?php
+        $conn = mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_db);
+        if ($conn->connect_error) {
+            die("connection fail:" . $conn->connect_error);
+        }
+
+        $sql = "CREATE TABLE IF NOT EXISTS login12 (
+            id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            statuscode VARCHAR(30) NOT NULL UNIQUE,
+            status1 VARCHAR(30) NOT NULL,
+            share VARCHAR(30) NOT NULL,
+            date1 VARCHAR(30) NOT NULL,
+            permissionType VARCHAR(60) NOT NULL
+
+            )ENGINE=InnoDB DEFAULT CHARSET=utf8 ";
+        if ($conn->query($sql) === TRUE) {
+            echo "Table created successfully!<br>";
+        } else {
+            echo "Create table failed: " . $conn->error;
+        }
+
+        $sql = "INSERT INTO login12(statuscode, status1, share, date1, permissionType) VALUES ('$m','$n','$p','$o','$hobby')";
+       
+        
+        if (!(mysqli_query($conn, $sql))) {
+            echo "Insert fail;<br>";
+            echo mysqli_error($conn);
+        } 
+
+        else {
+            echo "Insert Successfully！<br>";
+            echo "<a href='http://yxn9194.cmslamp14.aut.ac.nz/assign1/searchstatusform.html' target='_black' >Search Status</a><br>";
+        }
+
+
+    }
+    ?>
+
+</body>
+
+</html>
---
+++ Web Develop assign/searchstatusprocess.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    .father {
+        position: relative;
+        height: 600px;
+        border: 2px solid black;
+    }
+
+    .a1 {
+        position: absolute;
+        bottom: 20px;
+        right: 2px;
+    }
+
+    .a2 {
+        position: absolute;
+        bottom: 20px;
+        left: 1px;
+    }
+</style>
+
+<body>
+    <div class="father">
+        <?php
+        $sql_host = "cmslamp14.aut.ac.nz";
+        $sql_user = "yxn9194";
+        $sql_pass = "";
+        $sql_db = "yxn9194";
+
+        $conn = mysqli_connect($sql_host, $sql_user, $sql_pass, $sql_db);
+        if ($conn->connect_error) {
+            die("connection fail:" . $conn->connect_error);
+        }
+        
+        $sql1 = "CREATE TABLE IF NOT EXISTS login12 (
+        
+        statuscode VARCHAR(30) NOT NULL PRIMARY KEY,
+        status1 VARCHAR(30) NOT NULL,
+        share VARCHAR(30) NOT NULL,
+        date1 VARCHAR(30) NOT NULL,
+        permissionType VARCHAR(30) NOT NULL
+
+        )ENGINE=InnoDB DEFAULT CHARSET=utf8 ";
+
+        if (!(mysqli_query($conn, $sql1))) {
+            echo "Insert fail;";
+            echo mysqli_error($conn);
+        } else {
+            echo "";
+        }
+        $Search = $_GET["Search"];
+        $sql1 = "select * from login12 where status1 like '%" . $Search . "%' OR statuscode like '%" . $Search . "%'";
+        
+        $result = mysqli_query($conn, $sql1);
+        if (!$result) {
+            die('Search fail: ' . mysqli_error($conn));
+        }
+        echo "<h2>Status Information</h2>";
+        
+        while ($row = mysqli_fetch_array($result)) {
+            echo "<tr>";
+            echo "<table border='0'><tr><td>Status: </td><td>{$row['statuscode']}</td></tr>";
+            echo "<table border='0'><tr><td>Statuscode: </td><td>{$row['status1']}</td></tr>";
+            echo "<table border='0'><tr><td>Share: </td><td>{$row['share']}</td></tr>";
+            echo "<table border='0'><tr><td>Date Posted: </td><td>{$row['date1']}</td></tr>";
+            echo "<table border='0'><tr><td>Permission: </td><td>{$row['permissionType']}</td></tr>";
+            echo "</tr>";
+        }
+
+
+        ?>
+        <a href="http://yxn9194.cmslamp14.aut.ac.nz/assign1/index.html" target="_black" class="a1">Return to Homepage</a>
+        <a href="http://yxn9194.cmslamp14.aut.ac.nz/assign1/searchstatusform.html" target="_black" class="a2">Search Status</a>
+    </div>
+</body>
+
+</html>
